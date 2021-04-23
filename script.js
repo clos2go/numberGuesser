77e9a7a69967755bc90eb5d635fcf01c77682539
lopezlbj <carlos.lopez@austinisd.org>
2021-04-23 08:02:09 -0500

    generate targert

--- script.js
+++ script.js
@@ -2,5 +2,18 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+let generateTarget = () => {
+  currentRoundNumber = currentRoundNumber = Math.floor(Math.random() * 10);
+  return currentRoundNumber;
+};
+// console.log(generateTarget());
 
+let compareGuess = (userGuess, cpuGuess, secretNum) => {
+  const userDiff = Math.abs(userGuess) - Math.abs(secretNum);
+  const cpuDiff = Math.abs(cpuGuess) - Math.abs(secretNum);
+  // if (Math.abs(userGuess) - )
+  console.log(`user`, userDiff);
+  console.log(`cpu`, cpuDiff);
+};
+
+compareGuess(2, 9, generateTarget());
